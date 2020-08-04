2226a3b94c2fe18268f2f9b42a24ccce0feeeeb3
DaChuan11 <444978211@qq.com>
2020-08-04 17:28:29 +0800

    第二天

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navigation style="height:100%"></navigation>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import navigation from './components/navigation.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    navigation
   }
 }
 </script>
 
 <style lang="scss">
+*{
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  position: relative;
 }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
-Vue.use(ElementUI);
-Vue.config.productionTip = false;
+import slide from './components/slide.vue';
+import popup from './components/popup.vue';
+import echarts from 'echarts';
 
+Vue.use(ElementUI);
+Vue.component('slide', slide);
+Vue.component('popup', popup);
+
+Vue.config.productionTip = false;
+Vue.prototype.$echarts = echarts;
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
